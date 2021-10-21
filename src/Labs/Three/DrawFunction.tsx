364bb4a1bdba85863df5707a3374cea991e2eedd
iamabigartist <1548016008@qq.com>
2021-10-21 15:52:33 +0800

    Another bug that the red expasion will disappear

--- src/Labs/Three/DrawFunction.tsx
+++ src/Labs/Three/DrawFunction.tsx
@@ -1,6 +1,6 @@
 import {FC} from "react";
 import {Canvas} from "@react-three/fiber";
-import {acos, chain, compile, derivative, evaluate, exp, parse, parser, string} from "mathjs";
+import {acos, chain, compile, derivative, evaluate, exp, parse, parser, simplify, string} from "mathjs";
 import {Line, OrbitControls, useHelper} from "@react-three/drei";
 import {ArrowHelper, Vector2, Vector3} from "three";
 import {parseOptions} from "leva/dist/declarations/src/utils";
@@ -37,7 +37,7 @@ export function TaylorExpansion(f: string, variable: string, x0: number, order: 
     let derivatives = derivative_0_to_n(f, variable, order);
     let expansion = "";
     for (let i = 0; i <= order; i++) {
-        expansion += `+((${variable}-${x0})^${order})*${derivatives[i]}/${order}!`;
+        expansion += `+((${variable}-${x0})^${i})*${derivatives[i].evaluate({x: x0})}/${i}!`;
     }
     return expansion;
 }
@@ -65,22 +65,32 @@ export function Array_V2TOV3(array_V2: Vector2[]) {
 
 const DrawFunction: FC = () => {
 
-    let ori_f = "sin(x)+1/2*sin(2x)"
+    let ori_f = "sin((1/(x+0.01))^2)";
     let order = 5;
     let range: [number, number] = [-5, 5];
     let step = 0.01;
 
-    let cur_curve = compile(TaylorExpansion(ori_f, "x", 0, order));
+    let cur_curve_string = TaylorExpansion(ori_f, "x", 0, order);
+    let cur_curve = compile(simplify(cur_curve_string).toString());
     let cur_curve_points = Array_V2TOV3(SampleFx(
         (x) =>
             cur_curve.evaluate({x: x}),
         range, step));
 
+    let ori_curve = compile(simplify(ori_f).toString());
+    let ori_curve_points = Array_V2TOV3(SampleFx(
+        (x) =>
+            ori_curve.evaluate({x: x}),
+        range, step));
+
     return (<div>
         <div>
+            <h1>{simplify(cur_curve_string).toString()}</h1>
+            <h1>{cur_curve_string}</h1>
         </div>
-        <Canvas style={{top: "100px", height: "700px"}}>
-            <Line points={cur_curve_points}/>
+        <Canvas style={{top: "100px", height: "700px"}} gl={{antialias: true}}>
+            <Line points={cur_curve_points} color={"red"}/>
+            <Line points={ori_curve_points}/>
             <OrbitControls enableRotate={false} maxZoom={0} minZoom={0}/>
         </Canvas>
     </div>);

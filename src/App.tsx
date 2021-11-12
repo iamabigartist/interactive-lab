f0083d22a1d5bcf70413c148b31b58a44455c117
iamabigartist <1548016008@qq.com>
2021-11-12 22:15:31 +0800

    export demoshow

--- src/App.tsx
+++ src/App.tsx
@@ -1,41 +1,14 @@
 import React, {useState} from 'react';
 import "rsuite/dist/rsuite.min.css";
 import './App.css';
-import {MyCanvas} from "./Labs/Three/MyCanvas";
-import {Dropdown} from "rsuite";
-import {OneStore} from "./Labs/States/ZustandStore/ZustandLab";
-import DrawFunction from "./Labs/Three/DrawFunction";
-
+import DemoShow from "./Utilities/DemoShow";
 function App() {
 
-    const [display_i, setDisplay_i] = useState(2);
-    const display_items = ([
-        {name: "plain THREE canvas", item: <MyCanvas/>},
-        {name: "Zustand", item: <OneStore/>},
-        {name: "THREE draw function", item: <DrawFunction/>},
-        {
-            name: "Test End", item:
-                <div>
-                    <h1>This repo is used to test all kinds of interactive packages for our website.</h1>
-                    <a href={"https://github.com/64cksp4c3"}>Studio HomePage</a>
-                </div>
-        }
-    ]);
+
 
     return (
         <div className="App">
-            {display_items[display_i].item}
-            <Dropdown
-                title={display_items[display_i].name}
-                activeKey={display_i}
-                style={{position: "fixed", top: "5%", left: "5%"}}
-                onSelect={eventKey => {
-                    setDisplay_i(eventKey);
-                }}
-            >
-                {display_items.map((value, index) =>
-                    <Dropdown.Item key={index} eventKey={index}>{value.name} </Dropdown.Item>)}
-            </Dropdown>
+            <DemoShow/>
         </div>
     );
 }
---
+++ src/Utilities/DemoShow.tsx
@@ -0,0 +1,46 @@
+import React, {FC, useState} from 'react';
+import "rsuite/dist/rsuite.min.css";
+import {MyCanvas} from "../Labs/Three/MyCanvas";
+import {Dropdown} from "rsuite";
+import {OneStore} from "../Labs/States/ZustandStore/ZustandLab";
+import DrawFunction from "../Labs/Three/DrawFunction";
+
+const DemoShow: FC = () => {
+
+    const [display_i, setDisplay_i] = useState(2);
+    const display_demos = ([
+        {name: "plain THREE canvas", item: <MyCanvas/>},
+        {name: "Zustand", item: <OneStore/>},
+        {name: "THREE draw function", item: <DrawFunction/>},
+        {
+            name: "Test End", item:
+                <div>
+                    <h1>This repo is used to test all kinds of interactive packages for our website.</h1>
+                    <a href={"https://github.com/64cksp4c3"}>Studio HomePage</a>
+                </div>
+        }
+    ]);
+
+    return (
+        <div>
+            {display_demos[display_i].item}
+
+            {/*The demo selector*/}
+            <Dropdown
+                title={display_demos[display_i].name}
+                activeKey={display_i}
+                style={{position: "fixed", top: "5%", left: "5%"}}
+                onSelect={eventKey => {
+                    setDisplay_i(eventKey);
+                }}
+            >
+                <div>
+                    {display_demos.map((value, index) =>
+                        <Dropdown.Item key={index} eventKey={index}>{value.name} </Dropdown.Item>)}
+                </div>
+            </Dropdown>
+        </div>
+    );
+}
+
+export default DemoShow;
